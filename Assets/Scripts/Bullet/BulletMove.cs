4e6e590b0453a5c473a180a757da5ab4975cd726
Dionovan Ramadhani <80899280+kizaruu27@users.noreply.github.com>
2023-10-16 15:50:54 +0700

    feat: shoot mechanics with RPC

---
+++ Assets/Scripts/Bullet/BulletMove.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankWars3D
+{
+    public class BulletMove : MonoBehaviour
+    {
+        [SerializeField] private float bulletSpeed = 5f;
+        [SerializeField] private float bulletLifeTime = 8f;
+
+        private void Start()
+        {
+            Destroy(gameObject, bulletLifeTime);
+        }
+
+        private void Update()
+        {
+            transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
+        }
+    }
+}
+
--- Assets/Scripts/Player/Turret.cs
+++ Assets/Scripts/Player/Turret.cs
@@ -12,28 +12,52 @@ namespace TankWars3D
         public float rotationSpeed = 5.0f;
         [SerializeField] private Transform shootPoint;
         [SerializeField] private PhotonView view;
+        [SerializeField] private InputReader inputReader;
+        [SerializeField] private GameObject bulletPrefab;
+
+        private void OnEnable()
+        {
+            inputReader.OnShootEvent += Shoot;
+        }
+
+        private void OnDisable()
+        {
+            inputReader.OnShootEvent -= Shoot;
+        }
 
         private void Update()
         {
-            if (view.IsMine)
-                RotateTurret();
+            RotateTurret();
         }
 
         void RotateTurret()
         {
-            Vector3 mousePosition = Input.mousePosition;
-            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 20.0f));
-        
-            Vector3 direction = mouseWorldPosition - transform.position;
-            direction.y = 0.0f;
-        
-            if (direction != Vector3.zero)
+            if (view.IsMine)
             {
-                // Rotate the object to face the cursor (y-axis only)
-                Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
-                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+                Vector3 mousePosition = Input.mousePosition;
+                Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 20.0f));
+        
+                Vector3 direction = mouseWorldPosition - transform.position;
+                direction.y = 0.0f;
+        
+                if (direction != Vector3.zero)
+                {
+                    Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+                }
             }
+        }
 
+        void Shoot()
+        {
+            if (view.IsMine)
+                view.RPC("RpcShoot", RpcTarget.All);
+        }
+
+        [PunRPC]
+        void RpcShoot()
+        { 
+            Instantiate(bulletPrefab, shootPoint.position, shootPoint.rotation);
         }
     }
 }
