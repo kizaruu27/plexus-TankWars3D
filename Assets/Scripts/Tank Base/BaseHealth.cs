14690e2a420669e9094c806efd7e2235232c5512
Dionovan Ramadhani <80899280+kizaruu27@users.noreply.github.com>
2023-10-22 00:02:41 +0700

    feat: add base mechanics

---
+++ Assets/Scripts/Tank Base/BaseHealth.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TankWars3D
+{
+    public class BaseHealth : MonoBehaviour
+    {
+        public enum BaseState
+        {
+            MasterBase,
+            ClientBase
+        }
+        
+        [SerializeField] BaseState baseState;
+        
+        [SerializeField] private int maxBaseHealth;
+        private int currentBaseHealth;
+
+        [SerializeField] private Slider healthSlider;
+        [SerializeField] private GameObject gameOverUI;
+        [SerializeField] private TextMeshProUGUI winText;
+
+        private void Awake()
+        {
+            currentBaseHealth = maxBaseHealth;
+            healthSlider.maxValue = maxBaseHealth;
+        }
+
+        private void Update()
+        {
+            healthSlider.value = currentBaseHealth;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Bullet"))
+            {
+                TakeDamage();
+                Destroy(other.gameObject);
+            }
+        }
+
+        void TakeDamage()
+        {
+            currentBaseHealth--;
+
+            if (currentBaseHealth <= 0)
+            {
+                gameOverUI.SetActive(true);
+
+                switch (baseState)
+                {
+                    case BaseState.MasterBase:
+                        if (!PhotonNetwork.IsMasterClient)
+                            winText.text = "You Win";
+                        else
+                            winText.text = "You Lose";
+                        break;
+                    case BaseState.ClientBase:
+                        if (PhotonNetwork.IsMasterClient)
+                            winText.text = "You Win";
+                        else
+                            winText.text = "You Lose";
+                        break;
+                }
+                
+                Destroy(gameObject);
+            }
+        }
+    }
+}
