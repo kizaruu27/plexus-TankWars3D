804b793e401ed7f7d7551a1592dd25beb06765a8
Dionovan Ramadhani <80899280+kizaruu27@users.noreply.github.com>
2023-10-22 18:44:05 +0700

    feat: add destructable wall (prototype)

---
+++ Assets/Scripts/Walls/WallsBehaviour.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallsBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    enum State
+    {
+        Destructable,
+        Undestructable
+    }
+
+    [SerializeField] private State wallState;
+    
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("MasterBullet") || col.CompareTag("ClientBullet"))
+        {
+            Destroy(col.gameObject);
+            
+            switch (wallState)
+            {
+                case State.Destructable:
+                    Destroy(gameObject);
+                    break;
+                case State.Undestructable:
+                    break;
+            }
+        }
+        
+        
+        
+    }
+}
