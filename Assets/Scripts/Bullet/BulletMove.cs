9592264132d1317c9730b5f357066abbe5e76017
Dionovan Ramadhani <80899280+kizaruu27@users.noreply.github.com>
2023-10-23 05:06:24 +0700

    feat: add bullet particle system

--- Assets/Scripts/Bullet/BulletMove.cs
+++ Assets/Scripts/Bullet/BulletMove.cs
@@ -7,6 +7,7 @@ namespace TankWars3D
     {
         [SerializeField] private float bulletSpeed = 5f;
         [SerializeField] private float bulletLifeTime = 8f;
+        [SerializeField] private ParticleSystem bulletExplosion;
 
         private void Start()
         {
@@ -16,6 +17,11 @@ namespace TankWars3D
         private void Update()
         {
             transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
+        }
+
+        private void OnDestroy()
+        {
+            Instantiate(bulletExplosion, transform.position, Quaternion.identity);
         }
     }
 }
